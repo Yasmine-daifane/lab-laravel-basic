f61416093d75359138629f2779c4d08c08f60660
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2023-12-27 16:26:25 +0100

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+use App\Models\Project;
+use App\Http\Requests\FormTaskRequest;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $Tasks = Task::with('project')->paginate(4);
+        return view('Tasks.index', compact('Tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $Projects = Project::all();
+        return view('Tasks.create', compact('Projects'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(FormTaskRequest $request)
+    {
+        // dd($request->validated());
+        Task::create($request->validated());
+        return redirect('/')->with('success', 'Tâche créée avec succès !');
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task)
+    {
+        $Projects = Project::all();
+
+        return view('Tasks.edit', compact('task', 'Projects'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(FormTaskRequest $request, Task $task)
+    {
+        $task->update($request->validated());
+        return redirect('/')->with('success', 'Tâche update avec succès !');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FormTaskRequest $request, Task $task)
+    {
+        Task::create($request->validated());
+        return redirect('/')->with('success', 'Tâche destoryed avec succès !');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+
+Route::get('/', [TaskController::class, 'index'])->name('index');
+
+Route::get('create', [TaskController::class,'create'])->name('create');
+
+Route::post('store', [TaskController::class, 'store'])->name('store');
+
+Route::get('{task}/edit', [TaskController::class, 'edit'])->name('edit');
+Route::put('{task}/update', [TaskController::class, 'update'])->name('update');
+Route::put('{task}/destroy', [TaskController::class, 'destroy'])->name('destroy');
